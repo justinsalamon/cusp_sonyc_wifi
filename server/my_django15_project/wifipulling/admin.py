efd67cd42f15d6eb1764342f0bb805d4c863b16e
Nicholas Hagans <nah364@nyu.edu>
2015-04-16 19:18:48 -0400

    Merges ingestion project and wifipulling project into one project

--- server/my_django15_project/wifipulling/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from wifipulling.models import WifiScan
-from django.contrib import admin
-admin.site.register(WifiScan)
--- server/sonyc_ingestion_project/wifipulling/views.py
+++ server/sonyc_ingestion_project/wifipulling/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from wifipulling.models import WifiScan
+from ingestion.models import WifiScan
 import json
 import datetime
 def index(request):

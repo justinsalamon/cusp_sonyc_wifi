9512af00360cf5c751bd290eb08197983e4c1053
yaliu2015 <yl3268@nyu.edu>
2015-04-17 11:47:58 -0400

    wifipulling parameters

--- server/sonyc_ingestion_project/wifipulling/views.py
+++ server/sonyc_ingestion_project/wifipulling/views.py
@@ -5,42 +5,106 @@ from django.http import HttpResponse
 from ingestion.models import WifiScan
 import json
 import datetime
+import time
 def index(request):
 #     return HttpResponse("Hello. This is a test.")
 
 #     scan_list = WifiScan.objects.filter(ssid='nyu').values()
-    ss_id = request.GET.get('ssid', '')
-    if (ss_id == ''):
-        scan_dict = WifiScan.objects.all().values()
-    else:
-        scan_dict = WifiScan.objects.filter(ssid=ss_id).values()
-
-        
+    batch = 10 #default
+    offset = 0 #default
+    b_size = request.GET.get('batch', '')
+    off_size = request.GET.get('offset', '')
+    if (b_size != ''):
+        try:
+            batch = int(b_size)
+        except:
+            pass
+    if (off_size != ''):
+        try:
+            offset = int(off_size)
+        except:
+            pass    
+    idx_start = offset
+    idx_end = offset + batch
+    is_full_size = True if (b_size==''and off_size=='') else False
+    
+    q_idx = request.GET.get('idx', '')
+    q_lat = request.GET.get('lat', '')
+    q_lng = request.GET.get('lng', '')
+    q_radius = request.GET.get('radius', '')
+    q_acc = request.GET.get('acc', '')
+    q_alt = request.GET.get('altitude', '')
+    q_startdate = request.GET.get('startdate', '')
+    q_enddate = request.GET.get('enddate', '')
+    q_dev_mac = request.GET.get('device_mac', '')
+    q_app_v = request.GET.get('app_version', '')
+    q_dro_v = request.GET.get('droid_version', '')
+    q_dev_m = request.GET.get('device_model', '')
+    q_ssid = request.GET.get('ssid', '')
+    q_bssid = request.GET.get('bssid', '')
+    q_caps = request.GET.get('caps', '')
+    q_lvl = request.GET.get('level', '')
+    q_frq = request.GET.get('freq', '')
+    
+    query_set = None
+    try:
+        query_set = WifiScan.objects.all()
+    except:
+        pass
+    
     response_data = []
+    if (query_set != None):
+        if (q_idx != ''): # int
+            try:
+                query_set = query_set.filter(idx=q_idx)
+            except:
+                pass
+        if (q_acc != ''): #float
+            try:
+                query_set = query_set.filter(acc__gte=q_acc)
+            except:
+                pass
+        if (q_alt != ''): #double
+            try:
+                query_set = query_set.filter(altitude__gte=q_alt)
+            except:
+                pass
+        if (q_startdate != ''):
+            try:
+                mth, day, year = q_startdate.split('-',2)
+                dt = datetime.date(int(year), int(mth), int(day))
+                t_stamp = time.mktime(dt.timetuple()) * 1000
+                query_set = query_set.filter(time__gte=t_stamp)
+            except:
+                pass
+        if(q_ssid != ''):
+            query_set = query_set.filter(ssid=q_ssid)
+        if(q_caps != ''):
+            query_set = query_set.filter(caps__contains=q_caps)
+        
+        #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        if (is_full_size == False):
+            query_set = query_set[idx_start:idx_end]
+         
+        for record in query_set:
+            data = {}
+            data['idx']=record.idx
+            data['lat']=record.lat
+            data['lng']=record.lng
+            data['acc']=record.acc
+            data['altitude']=record.altitude      
+            data['time']=(datetime.datetime.fromtimestamp(record.time/1000)).strftime('%m-%d-%Y %H:%M:%S')
+            data['device_mac']=record.device_mac
+            data['app_version']=record.app_version
+            data['droid_version']=record.droid_version
+            data['device_model']=record.device_model
+            data['ssid']=record.ssid
+            data['bssid']=record.bssid
+            data['caps']=record.caps
+            data['level']=record.level
+            data['freq']=record.freq
      
-    i = 0
-    for record in scan_dict:
-        data = {}
-        data['idx']=record['idx']
-        data['lat']=record['lat']
-        data['lng']=record['lng']
-        data['acc']=record['acc']
-        data['altitude']=record['altitude']       
-        data['time']=(datetime.datetime.fromtimestamp(record['time']/1000)).strftime('%m-%d-%Y %H:%M:%S')
-        data['device_mac']=record['device_mac']
-        data['app_version']=record['app_version']
-        data['droid_version']=record['droid_version']
-        data['device_model']=record['device_model']
-        data['ssid']=record['ssid']
-        data['bssid']=record['bssid']
-        data['caps']=record['caps']
-        data['level']=record['level']
-        data['freq']=record['freq']
- 
-#         i = 1 + i
-#         if (i == 40000):
-#             break
-        response_data.append(data)
+            response_data.append(data)
     return HttpResponse(json.dumps(response_data), content_type="application/json")
 #     return HttpResponse(scan_list)#, mimetype='application/json')
 #     return HttpResponse(scan_list)

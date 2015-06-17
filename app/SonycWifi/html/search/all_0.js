ca2a8ce9e2e722a3106fcc24e705a889bfa11429
Nicholas Hagans <nah364@nyu.edu>
2015-06-17 00:11:48 -0400

    Fixes #17 by increasing timeout. Also changed IDE so new configuration

--- app/SonycWifi/html/search/all_0.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['changefilename',['changeFileName',['../classcom_1_1example_1_1sonycwifii_1_1_main_activity_1_1upload_async_task.html#a1d83f699b154610d8eee5cd0a8dd7267',1,'com::example::sonycwifii::MainActivity::uploadAsyncTask']]],
-  ['com',['com',['../namespacecom.html',1,'']]],
-  ['example',['example',['../namespacecom_1_1example.html',1,'com']]],
-  ['sonycwifii',['sonycwifii',['../namespacecom_1_1example_1_1sonycwifii.html',1,'com::example']]]
-];
--- app/SonycWifi/html/search/all_2.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['getprovider',['getProvider',['../classcom_1_1example_1_1sonycwifii_1_1_main_activity_1_1my_async_task.html#a64afdd179e89cc278cd5002a62637d3e',1,'com::example::sonycwifii::MainActivity::myAsyncTask']]]
-];
